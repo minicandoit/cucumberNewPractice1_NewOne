9f082dd017c9aa9a558e77fa6df5ec834c53211a
minicandoit <74155239+minicandoit@users.noreply.github.com>
2021-05-20 01:36:06 -0400

    Initial commit

---
+++ src/test/java/com/interview/pages/NewTapPage.java
@@ -0,0 +1,20 @@
+package com.interview.pages;
+
+import com.interview.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.WeakHashMap;
+
+public class NewTapPage {
+    public NewTapPage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+    @FindBy(xpath = "//a[@target='_blank']")
+    public WebElement ClickHere;
+
+    @FindBy(xpath = "//h3")
+    public WebElement newWindowText;
+
+}
